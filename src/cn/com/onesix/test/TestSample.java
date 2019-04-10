15ccc4c60fbe5ab376d1b506ecac8a30675f6528
fengyuan <fengyuan@caict.ac.cn>
2019-04-10 17:01:38 +0800

    更改main实现

--- src/cn/com/onesix/test/TestSample.java
+++ src/cn/com/onesix/test/TestSample.java
@@ -4,8 +4,8 @@ import cn.com.onesix.Sample;
 
 public class TestSample {
 	public static void main(String[] args) {
-		new Sample("abc","girl");
-		System.out.println("hello hub");
+		Sample sample = new Sample("abc","girl");
+		System.out.println(sample.toString());
 	}
 
 }
