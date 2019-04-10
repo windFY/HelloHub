1cea3e7e7f25f5ab544e968b43fdea6974ce8126
fengyuan <fengyuan@caict.ac.cn>
2019-04-10 10:57:46 +0800

    修改sample类

--- src/cn/com/onesix/Sample.java
+++ src/cn/com/onesix/Sample.java
@@ -2,8 +2,20 @@ package cn.com.onesix;
 
 public class Sample {
 	private String name;
-	public Sample(String name){
+	private String sex;
+	public Sample(String name,String sex){
 		this.name = name;
+		this.sex = sex;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "Sample [name=" + name + ", sex=" + sex + "]";
 	}
 	public String getName() {
 		return name;
--- src/cn/com/onesix/test/TestSample.java
+++ src/cn/com/onesix/test/TestSample.java
@@ -4,7 +4,7 @@ import cn.com.onesix.Sample;
 
 public class TestSample {
 	public static void main(String[] args) {
-		new Sample("abc");
+		new Sample("abc","girl");
 		System.out.println("hello hub");
 	}
 
