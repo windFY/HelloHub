2b9f7cfe714f61ff7be927fb5dd8c30d040da021
fengyuan <fengyuan@caict.ac.cn>
2019-04-10 10:50:45 +0800

    添加两个java文件

---
+++ src/cn/com/onesix/Sample.java
@@ -0,0 +1,14 @@
+package cn.com.onesix;
+
+public class Sample {
+	private String name;
+	public Sample(String name){
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/cn/com/onesix/test/TestSample.java
@@ -0,0 +1,11 @@
+package cn.com.onesix.test;
+
+import cn.com.onesix.Sample;
+
+public class TestSample {
+	public static void main(String[] args) {
+		new Sample("abc");
+		System.out.println("hello hub");
+	}
+
+}
