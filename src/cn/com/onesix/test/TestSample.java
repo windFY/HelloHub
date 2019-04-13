a0dcfca4a01963bf9517da39a0f9740b0a3edf40
fengyuan <fengyuan@caict.ac.cn>
2019-04-13 12:11:57 +0800

    代码中添加日志操作

--- src/cn/com/onesix/test/TestSample.java
+++ src/cn/com/onesix/test/TestSample.java
@@ -1,10 +1,17 @@
 package cn.com.onesix.test;
 
+
+import org.apache.log4j.Logger;
+
 import cn.com.onesix.Sample;
 
 public class TestSample {
+	private static Logger logger = Logger.getLogger(TestSample.class);  
 	public static void main(String[] args) {
 		Sample sample = new Sample("abc","girl");
+		logger.info("info---");
+		logger.warn("warn---");
+		logger.error("error---");
 		System.out.println(sample.toString());
 	}
 
